b807450e442e9967b74eee0654780ac9412ec53e
Scorza9999 <oliva.scorza@gmail.com>
2026-06-10 17:37:20 +0200

    created loadingScreen

---
+++ app/loadingScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function loadingScreen() {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+}
+
+export default loadingScreen;
