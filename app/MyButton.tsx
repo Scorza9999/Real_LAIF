af893ceb82f43f9daacb818935230043940d7321
Scorza9999 <oliva.scorza@gmail.com>
2026-06-11 17:38:13 +0200

    removed console.log for debug and added comments

--- app/MyButton.tsx
+++ app/MyButton.tsx
@@ -8,30 +8,37 @@ function MyButton() {
   var message: String = "";
   const os: string = Platform.OS;
 
+  // changes the message based on bool's value
   if (bool) {
     message = "True";
   } else {
     message = "False";
   }
 
+  // for the web
+  // TODO: use the RandomBooleanModule.web.ts instead
   function randomWeb(): boolean {
     return Math.random() > 0.5;
   }
 
+  // if on the web, just performs a math.random (for testing purposes)
   async function handleClick() {
     if (os === "web") {
       setBool(randomWeb());
     } else {
+      // this means it's on android
+
       try {
         const nativeResult = await Random();
         setBool(nativeResult);
-        console.log("Fatto");
       } catch (error) {
-        console.error("ERRORE NEL CARICAMENTO");
+        // this should be handled in the UI
+        console.error("ERRORE");
       }
     }
   }
 
+  // shows the message
   return (
     <Pressable onPress={handleClick}>
       <Text>{message}</Text>
