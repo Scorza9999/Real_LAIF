bcb8a1b807a98f0b6c3506b2318830204d1c32e7
Scorza9999 <oliva.scorza@gmail.com>
2026-06-11 16:38:10 +0200

    updated gradle to the correct java version
    created the RandomBoolean module

--- modules/random-boolean/android/src/main/java/expo/modules/randomboolean/RandomBooleanModule.kt
+++ modules/random-boolean/android/src/main/java/expo/modules/randomboolean/RandomBooleanModule.kt
@@ -2,9 +2,33 @@ package expo.modules.randomboolean
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RandomBooleanModule : Module() {
-  override fun definition() = ModuleDefinition {
-    Name("RandomBoolean")
-  }
+    private val moduleScope = CoroutineScope(Dispatchers.Default)
+
+    override fun definition() = ModuleDefinition { 
+        Name("RandomBoolean")
+
+        AsyncFunction("Random") { promise: expo.modules.kotlin.Promise -> 
+            moduleScope.launch {
+                try {
+                    val result = withContext(Dispatchers.Default) {
+                        executeRandom()
+                    }
+                    promise.resolve(result)
+                } catch (e: Exception) {
+                    promise.reject("ERR_RANDOM_BOOL_ERROR", e.message, e)
+                }
+            } 
+        }
+    }
+
+    // Safely placed inside the class boundaries
+    private fun executeRandom(): Boolean {
+        return Math.random() > 0.5
+    }
 }
