074f4c9328fda3732609c0363c42983e7835c834
Scorza9999 <oliva.scorza@gmail.com>
2026-06-18 17:16:32 +0200

    added the image as a parameter inside kotlin. removed things we don't (yet) need

--- modules/random-boolean/android/src/main/java/expo/modules/randomboolean/RandomBooleanModule.kt
+++ modules/random-boolean/android/src/main/java/expo/modules/randomboolean/RandomBooleanModule.kt
@@ -2,30 +2,21 @@ package expo.modules.randomboolean
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import kotlinx.coroutines.*
+// import kotlinx.coroutines.*
 
 class RandomBooleanModule : Module() {
-    private val moduleScope = CoroutineScope(Dispatchers.Default)
 
     override fun definition() = ModuleDefinition { 
         Name("RandomBoolean")
 
-        AsyncFunction("Random") { promise: expo.modules.kotlin.Promise -> 
-            moduleScope.launch {
-                try {
-                    val result = withContext(Dispatchers.Default) {
-                        executeRandom()
-                    }
-                    promise.resolve(result)
-                } catch (e: Exception) {
-                    promise.reject("ERR_RANDOM_BOOL_ERROR", e.message, e)
-                }
-            } 
+        AsyncFunction("Random") { uri: String -> executeRandom(uri)
         }
     }
 
     // insert code here
-    private fun executeRandom(): Boolean {
+    private fun executeRandom(uri: String): Boolean {
+        //TODO: make async and implement actual logic
+
         return Math.random() > 0.5
     }
 }
