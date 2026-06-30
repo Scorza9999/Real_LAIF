2a7241394031a7075cc43f5f5d3c2ea26bf6bdb5
Scorza9999 <oliva.scorza@gmail.com>
2026-06-30 14:31:58 +0200

    Added something to show to the user: as for now, it's just the image they uploaded and lorem ipsum

--- app/result/result_screen.tsx
+++ app/result/result_screen.tsx
@@ -1,4 +1,7 @@
+import { Text } from "@react-navigation/elements";
 import { useLocalSearchParams, useRouter } from "expo-router";
+import React from "react";
+import { Image, View } from "react-native";
 
 const result_screen = () => {
   // the image uri and the operation to be performed
@@ -8,4 +11,33 @@ const result_screen = () => {
   }>();
   const router = useRouter();
   const opcode = Number(param_opcode);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={{ uri: imageUri }}
+        style={{
+          height: 500,
+          width: 300,
+          resizeMode: "contain",
+          borderColor: "black",
+          borderWidth: 4,
+        }}
+      />
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+        explicabo, aliquid minima consequatur ipsa nulla quo voluptas non magnam
+        error quia incidunt nihil expedita nisi vel temporibus cupiditate
+        voluptate quibusdam!
+      </Text>
+    </View>
+  );
 };
+
+export default result_screen;
